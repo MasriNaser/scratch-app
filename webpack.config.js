35188b3de1a8ac54fd0995f4f6dd92bf3a770e53
Naser Masri <Naser.eng.85@hotmail.com>
2019-08-09 16:40:10 +0200

    loader css and sass

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,19 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ]
   },
   plugins: [
